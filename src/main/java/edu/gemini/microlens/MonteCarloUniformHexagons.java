52c469caff77234e7811f07104f4a52bb6705bc2
Sebastian Raaphorst <sraaphorst@gmail.com>
2020-03-03 14:38:00 -0300

    WIP.

--- src/main/java/edu/gemini/microlens/MonteCarloUniformHexagons.java
+++ src/main/java/edu/gemini/microlens/MonteCarloUniformHexagons.java
@@ -72,16 +72,26 @@ public class MonteCarloUniformHexagons extends JPanel {
         // These are in arcsec:
         // 1. hexagonRadius is the radius of the microlens.
         // 2. paddedHexagonRadius is the radius of the microlens with the padding beween them.
-        // microlenses.side and microlenses.side +padding are in arcsec, so we convert to pixels.
+        // microlenses.side and (microlenses.side + padding) / 2 are in arcsec, so we convert to pixels.
         hexagonRadius = (SIZE - PADDING) * microlenses.side;
-        paddedHexagonRadius = (SIZE - PADDING) * (microlenses.side + padding);
+        paddedHexagonRadius = (SIZE - PADDING) * (microlenses.side + padding / 2);
 
         // This is also in arcsec, and is the length of the bisection of the equilateral triangles making up the
         // hexagons. It does not include the padding.
-        double a = hexagonRadius * Math.sqrt(3);
+        final double hexagonBisector = hexagonRadius * Math.sqrt(3);
+        final double paddedHexagonBisector = paddedHexagonRadius * Math.sqrt(3);
 
         // Create the inner hexagon, and then create the concentric rings.
         hexagons = new ArrayList<>();
+
+        final RegularHexagon h = new RegularHexagon();
+        final AffineTransform transform = new AffineTransform();
+        transform.translate(200, 200);
+        transform.rotate(Math.PI / 1.3);
+        transform.scale(200, 200);
+//        transform.translate(-300, -500);
+        h.transform(transform);
+        hexagons.add(h);
 
         // This is a hexagon in ring 0, i.e. a solitary hexagon in the centre.
         final RegularHexagon hexagonRing0 = new RegularHexagon();
@@ -90,20 +100,46 @@ public class MonteCarloUniformHexagons extends JPanel {
         hexagonRing0.transform(trans0);
         hexagons.add(hexagonRing0);
 
-        // This is the first hexagon in ring 1.
+        // This is the top ring 1 in hexagon ring 1.
         final RegularHexagon hexagonRing1_0 = new RegularHexagon();
-        final AffineTransform trans1_0 = AffineTransform.getTranslateInstance(hexagonPoint, hexagonPoint - a - (paddedHexagonRadius - hexagonRadius));
+        final AffineTransform trans1_0 = AffineTransform.getTranslateInstance(hexagonPoint, hexagonPoint - hexagonBisector - (paddedHexagonRadius - hexagonRadius));
         trans1_0.scale(hexagonRadius, hexagonRadius);
         hexagonRing1_0.transform(trans1_0);
         hexagons.add(hexagonRing1_0);
 
-        // Rotate the hexagon in ring 1 to get its neighbour.
         final RegularHexagon hexagonRing1_1 = new RegularHexagon();
         final AffineTransform trans1_1 = new AffineTransform();
-        trans1_1.translate(hexagonPoint, hexagonPoint + a + (paddedHexagonRadius - hexagonRadius));
+        // Order of scale and rotate matters! rotate = 3 has no effect.
+        trans1_1.translate(hexagonPoint, hexagonPoint - hexagonBisector - (paddedHexagonRadius - hexagonRadius));
+        trans1_1.rotate(Math.PI / 1000.0, hexagonPoint, hexagonPoint);
         trans1_1.scale(hexagonRadius, hexagonRadius);
         hexagonRing1_1.transform(trans1_1);
         hexagons.add(hexagonRing1_1);
+
+//        // This is the bottom ring 1 in hexagon ring 1.
+//        final RegularHexagon hexagonRing1_1 = new RegularHexagon();
+//        final AffineTransform trans1_1 = new AffineTransform();
+//        trans1_1.translate(hexagonPoint, hexagonPoint + a + (paddedHexagonRadius - hexagonRadius));
+//        trans1_1.scale(hexagonRadius, hexagonRadius);
+//        hexagonRing1_1.transform(trans1_1);
+//        hexagons.add(hexagonRing1_1);
+//
+//        // This is the bottom ring 1 in hexagon ring 1.
+//        final RegularHexagon hexagonRing1_3 = new RegularHexagon();
+//        final AffineTransform trans1_3 = new AffineTransform();
+//        trans1_3.translate(hexagonPoint + 1.5 * paddedHexagonRadius, hexagonPoint - paddedHexagonRadius);
+//        trans1_3.scale(hexagonRadius, hexagonRadius);
+//        hexagonRing1_3.transform(trans1_3);
+//        hexagons.add(hexagonRing1_3);
+//
+//        // This is the bottom ring 1 in hexagon ring 1.
+//        final RegularHexagon hexagonRing1_2 = new RegularHexagon();
+//        final AffineTransform trans1_2 = new AffineTransform();
+//        trans1_2.translate(hexagonPoint + 1.5 * hexagonRadius, hexagonPoint + paddedHexagonRadius);
+//        trans1_2.scale(hexagonRadius, hexagonRadius);
+//        hexagonRing1_2.transform(trans1_2);
+//        hexagons.add(hexagonRing1_2);
+
 
 //        final RegularHexagon hexagonRing1_1 = new RegularHexagon();
 //        final AffineTransform trans1_1 = new AffineTransform();
@@ -157,7 +193,7 @@ public class MonteCarloUniformHexagons extends JPanel {
     public static void main(String[] args) {
         final JFrame frame = new JFrame("Hexagon");
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        final MonteCarloUniformHexagons hexagonPanel = new MonteCarloUniformHexagons(Microlenses.HIGH_RESOLUTION, 0.01);
+        final MonteCarloUniformHexagons hexagonPanel = new MonteCarloUniformHexagons(Microlenses.HIGH_RESOLUTION, 0.05);
         frame.add(hexagonPanel);
 
         final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
